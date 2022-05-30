8bb4f11966836f4a661f80a1e8ffde455910bdcd
vojta <vojtabern@gmail.com>
2022-05-30 18:36:15 +0200

    must figure out how to do pk

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('class/detail/', views.ClassDetailView.as_view(), name='class_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from polls.models import Class, Character, Player, Level
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
@@ -13,3 +14,12 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class ClassDetailView(DetailView):
+    # Nastavení požadovaného modelu
+    model = Class
+    # Pojmenování objektu, v němž budou šabloně předána data z modelu (tj. databázové tabulky)
+    context_object_name = 'class_detail'
+    # Umístění a název šablony
+    template_name = 'class/class_detail.html'
