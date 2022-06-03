ea7a86925cb156c4da7fbd21ec658be49e4b24d4
vojta <vojtabern@gmail.com>
2022-06-03 19:24:55 +0200

    done_details

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('character/<int:pk>', views.CharacterDetailView.as_view(), name='character_detail'),
-    path('class/<int:pk>', views.ClassDetailView.as_view(), name='class_detail'),
+    path('class/<str:pk>', views.ClassDetailView.as_view(), name='class_detail'),
 ]
